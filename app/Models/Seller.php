bbf39cf88cc3e514ac8f6ace940180b54150dbac
unknown <kamrankhan3.61k@gmail.com>
2025-10-06 21:19:25 +0500

    adding the models code which is used for database

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Seller extends Model
+class Seller extends Authenticatable
 {
-    //
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'business_name',
+        'is_approved',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'is_approved' => 'boolean',
+        ];
+    }
 }
